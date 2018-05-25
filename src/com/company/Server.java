2cb5fc9a2b5279205489bf8ab16a63ee703519dd
Sarefthief <Sarefthief@gmail.com>
2018-05-25 18:07:56 +0700

    Initial commit

---
+++ src/com/company/Server.java
@@ -0,0 +1,6 @@
+package com.company;
+
+import com.sun.net.httpserver.HttpServer;
+
+public class Server {
+}
---
+++ src/com/company/ServerApp.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class ServerApp {
+
+    public static void main(String[] args) {
+
+    }
+}
