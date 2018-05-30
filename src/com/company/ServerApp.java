5ddf596f1cf1eb82f24c54a5b6155ee7f1d65a99
Sarefthief <Sarefthief@gmail.com>
2018-05-30 19:21:22 +0700

    spaces deleted

--- src/com/company/ServerApp.java
+++ src/com/company/ServerApp.java
@@ -1,9 +1,6 @@
 package com.company;
-
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
-
 import java.io.File;
 import java.io.IOException;
 
